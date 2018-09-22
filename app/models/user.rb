951234f310709c1c45226fc1faf8289775a3ec03
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-09-22 21:09:46 +0700

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   has_many :tokens
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
   def self.authenticate(email, password)
     user = find_by(email: email).try(:authenticate, password)
     if user.present?
