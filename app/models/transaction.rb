f7487f34571f63f8b5fd440debac530710a6d46f
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-09-24 12:44:56 +0700

    Update transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,7 +10,7 @@ class Transaction < ApplicationRecord
 
   def initialize(args)
     super(args)
-    self.txref ||= SecureRandom.uuid
+    self.txref ||= Transaction.uuid
   end
 
   belongs_to :user
